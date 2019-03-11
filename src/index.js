29fb25beaa7417d0737e0dac2b866244c1f0943e
lihaichen <li8303@163.com>
2019-03-11 13:30:49 +0800

    加入dva

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const Index = () => {
-    return <div>Hello React!!!</div>;
-  };
-ReactDOM.render( <Index/>, document.getElementById('root'));
+import dva from 'dva';
+import React from 'react'
+import './index.less';
+import {browserHistory} from 'dva/router';
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// const Index = () => {
+//     return <div>Hello React!!!</div>;
+//   };
+// ReactDOM.render( <Index/>, document.getElementById('root'));
+
+const APP = (props)=>{
+  return <h1>hello world!</h1>;
+}
+
+const app = dva({
+  history: browserHistory
+});
+
+app.router(()=><APP/>);
+
+app.start('#root');
