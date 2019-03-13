9d66b520eab48bbf7bf1aff3f2f6fa36125ba362
lihaichen <li8303@163.com>
2019-03-13 12:23:14 +0800

    将react 分离出来

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,74 +1,80 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const config = require("../config.json");
-const assetsPath = path.resolve(__dirname, "../static");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const config = require('../config.json');
+const assetsPath = path.resolve(__dirname, '../static');
 const gitHash = config.debugConfig.gitHash;
 
 module.exports = {
-  entry: { main: ["./src/index.js"] },
+  entry: {main: ['./src/index.js']},
   output: {
     path: assetsPath,
-    filename: `js/[name]-${gitHash}.[hash:4].js`,
-    chunkFilename: `js/[name]-${gitHash}.[hash:4].js`,
-    publicPath: "/"
+    filename: `js/[name]-${gitHash}.[chunkhash:4].js`,
+    chunkFilename: `js/[name]-${gitHash}.[chunkhash:4].js`,
+    publicPath: '/'
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new CleanWebpackPlugin(), new HtmlWebpackPlugin({
       title: config.title,
-      template: "./template/index.html",
-      filename: path.resolve(assetsPath, "./index.html")
-    })
+      template: './template/index.html',
+      filename: path.resolve(assetsPath, './index.html')
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    // new webpack.LoaderOptionsPlugin({minimize: true})
   ],
   module: {
     rules: [
-      { test: /\.js[x]?$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.json$/, use: ["json-loader"] },
-      {
+      {test: /\.js[x]?$/, exclude: /node_modules/, use: ['babel-loader']},
+      {test: /\.json$/, use: ['json-loader']}, {
         test: /\.less$/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "less-loader" }
+          {loader: 'style-loader'}, {loader: 'css-loader'},
+          {loader: 'less-loader'}
         ]
       },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      { test: /\.(png|svg|jpg|gif)$/, use: ["url-loader"] },
+      {test: /\.css$/, use: [{loader: 'style-loader'}, {loader: 'css-loader'}]},
+      {test: /\.(png|svg|jpg|gif)$/, use: ['url-loader']},
       // 解析 MakeDown 文件
-      { test: /\.md$/, use: ["html-loader", "markdown-loader"] },
+      {test: /\.md$/, use: ['html-loader', 'markdown-loader']},
       // 解析数据资源
-      { test: /\.(csv|tsv)$/, use: ["csv-loader"] },
+      {test: /\.(csv|tsv)$/, use: ['csv-loader']},
       // 解析数据资源
-      { test: /\.xml$/, use: ["xml-loader"] },
+      {test: /\.xml$/, use: ['xml-loader']},
       // 解析 字体
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ["file-loader"] }
+      {test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']}
     ]
   },
   resolve: {
-    alias: { src: path.resolve(__dirname, "src") },
-    modules: [path.resolve(__dirname, "src"), "node_modules"]
+    alias: {src: path.resolve(__dirname, 'src')},
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
       minSize: 30000,
       maxSize: 0,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
-      automaticNameDelimiter: "~",
+      automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
+        react: {
+          test: /[\\/]?node_modules[\\/]react*[\\/]?/,
+          chunks: 'initial',
+          name: 'react',
+          priority: 20,
+        },
         vendor: {
           test: /node_modules/,
-          chunks: "initial",
-          name: "vendor",
-          priority: -10,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10,
           enforce: false
         },
-        default: { minChunks: 2, priority: -20, reuseExistingChunk: true }
+        default: {minChunks: 2, priority: -20, reuseExistingChunk: true}
       }
     }
   }
